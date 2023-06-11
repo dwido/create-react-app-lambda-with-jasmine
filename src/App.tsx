44c6dc7e56a24f54b4a062f9918e64f680a8e2c0
ngurevich <ngurevich@outbrain.com>
2023-06-11 18:14:22 +0300

    starting-together: React 18 & context & UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,24 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { useGameContext } from './store/ctx';
+import Button from './UI/Button';
 
-interface Symbol {
-  color: "red" | "black";
-  icon: "club" | "diamond" | "spade" | "heart";
-}
+const App = () => {
+	const ctx = useGameContext();
+	const onBtnClick = () => {
+		console.log('ctx', ctx);
+	};
 
-/* card 2
- * score 2
- * text '2'
- * order 2
- *
- * card J
- * text 'J'
- * order 11
- * score 10
-
- ** card Joker
- * text 'Joker'
- * order 0
- * score 0
- *
- */
-
-interface CardValue {
-  score: number;
-  text: string; // '2', '3' ..... '10', 'J', 'Q', 'K', 'A', 'Joker'
-  order: number; // 0 - 13
-}
-
-interface Card {
-  symbol: Symbol;
-  value: CardValue;
-}
-
-interface AppState {
-}
-type Deck = Card[];
-
-class App extends Component {
-  deck: Card[] | undefined;
-
-  constructor(props: AppState) {
-    super(props);
-    this.initializeDeck();
-  }
-  
-
-  initializeDeck() {
-    //deck: Card[] =
-    //  //initialize deck
-    //  //create deck
-    //  [
-    //    {
-    //      symbol: {
-    //        color: "red",
-    //        icon: "heart",
-    //      },
-    //      value: {
-    //        score: 2,
-    //        text: "2",
-    //        order: 2,
-    //      },
-    //    },
-    //  ];
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to hello everyone this is the
-            new yaniv.
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>
+					Edit <code>src/App.js</code> and save to hello everyone this is the
+					new yaniv.
+				</p>
+				<Button onClick={onBtnClick} txt='This is my new custom button' />
+			</header>
+		</div>
+	);
+};
 
 export default App;
